92bf4bc44e3d24248e0e6b188e378fb0ae86695b
Ingrid Alves <88692119+ingridalvesz@users.noreply.github.com>
2025-07-18 14:57:08 -0300

    #ModuloDeEntrada

---
+++ SalesMindAnalytics/Leitura de dados/bibliotecas.py
@@ -0,0 +1,8 @@
+#Primeiramente, importaremos as biblioteca
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+# Exporta os nomes
+__all__ = ["pd", "plt", "sns", "px"]
